244ee7d1d078dbd6fdb72c8acf53af1228d45dfd
HemanthPaila <pailahemanth2002@gmail.com>
2023-01-22 11:45:03 +0530

    started portfolio website

---
+++ src/Header.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./Header.css";
+const Header = () => {
+  return (
+    <div className="body">
+      <div className="main-body"></div>
+    </div>
+  );
+};
+export default Header;
