f13db0a7912ff14f042068cbd9d1b7ca1ec438da
HemanthPaila <pailahemanth2002@gmail.com>
2023-01-24 16:06:30 +0530

    modified education.js and education.css files

--- src/Education.js
+++ src/Education.js
@@ -8,6 +8,17 @@ const Education = () => {
         <div className="btech"></div>
         <div className="tenth"></div>
         <div className="inter"></div>
+        <div className="edu-cont__container">
+          <div className="edu-cont">
+            <p>
+              Lorem ipsum dolor sit amet. Aut rerum pariatur ex tenetur
+              veritatis aut aliquam unde est dolores unde. Ut sapiente dolor et
+              dolorem ratione a quis nulla ut saepe error ut nemo perspiciatis.
+              Et totam cumque et nihil quos qui corrupti praesentium qui placeat
+              voluptas aut odio nulla!
+            </p>
+          </div>
+        </div>
       </div>
     </div>
   );
