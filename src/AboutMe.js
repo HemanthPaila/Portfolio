84d9f82942847405b4fbdbe0f327569848127725
HemanthPaila <pailahemanth2002@gmail.com>
2023-01-24 19:25:26 +0530

    added media query into about section

--- src/AboutMe.js
+++ src/AboutMe.js
@@ -4,34 +4,34 @@ const AboutMe = () => {
   return (
     <div className="body1">
       <div className="main-body1">
-        <div className="main-content">
-          <div className="main-about__container">
-            <div className="about-content">ABOUT</div>
-          </div>
-          <div className="about">
-            <p>
-              I am Hemanth. I am pursuing a bachelor's degree in computer
-              science and engineering at KL University. I am motivated to
-              practice real-world problems daily and achieve big in the future.
-              My biggest aspiration is to become a full-stack web developer and
-              an app developer.
-            </p>
-          </div>
-          <div className="person-img"></div>
-          <div className="experience-img"></div>
-          <div className="exp-heading">EXPERIENCE</div>
-          <div className="exp-content">
-            <p>
-              I am Hemanth. I am pursuing a bachelor's degree in computer
-              science and engineering at KL University. I am motivated to
-              practice real-world problems daily and achieve big in the future.
-              My biggest aspiration is to become a full-stack web developer and
-              an app developer.
-            </p>
-          </div>
+        {/* <div className="main-content"> */}
+        <div className="main-about__container">
+          <div className="about-content">ABOUT</div>
+        </div>
+        <div className="about">
+          <p>
+            I am Hemanth. I am pursuing a bachelor's degree in computer science
+            and engineering at KL University. I am motivated to practice
+            real-world problems daily and achieve big in the future. My biggest
+            aspiration is to become a full-stack web developer and an app
+            developer.
+          </p>
+        </div>
+        <div className="person-img"></div>
+        <div className="experience-img"></div>
+        <div className="exp-heading">EXPERIENCE</div>
+        <div className="exp-content">
+          <p>
+            I am Hemanth. I am pursuing a bachelor's degree in computer science
+            and engineering at KL University. I am motivated to practice
+            real-world problems daily and achieve big in the future. My biggest
+            aspiration is to become a full-stack web developer and an app
+            developer.
+          </p>
         </div>
       </div>
     </div>
+    // </div>
   );
 };
 export default AboutMe;
